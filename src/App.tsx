423ae565f5c3f2fd149fc71d8e0efefba6ef160d
Aleksandr_Ruzaev <aleksandr.ruzaev@rokolabs.com>
2022-08-17 16:29:56 +0400

    Use router with mocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import { PublicLayout } from './layouts/public-layout';
+
 function App() {
-  return <div className="App"></div>;
+    return (
+        <div className="App">
+            <PublicLayout>
+                <Routes>
+                    {/* <Route path="/about" element={<About />}></Route> */}
+                    <Route path="/pricing/:id" element={<div>r1</div>}></Route>
+                    <Route path="/" element={<div>123</div>}></Route>
+                </Routes>
+            </PublicLayout>
+        </div>
+    );
 }
 
 export default App;
