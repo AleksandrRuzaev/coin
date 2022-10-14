70e72ed0d4f89d0cc439e18dbb7270d3a7e17b74
Aleksandr_Ruzaev <aleksandr.ruzaev@rokolabs.com>
2022-10-14 19:05:47 +0400

    Add recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import { PublicLayout } from './layouts/public-layout';
 import { Currency, Home, Settings, Statistics } from './pages';
 import { pages } from './utils/routes';
@@ -6,17 +7,25 @@ import { pages } from './utils/routes';
 function App() {
     return (
         <div className="App">
-            <PublicLayout>
-                <Routes>
-                    <Route path={pages.home.value} element={<Home />} />
-                    <Route
-                        path={pages.statistics.value}
-                        element={<Statistics />}
-                    />
-                    <Route path={pages.currency.value} element={<Currency />} />
-                    <Route path={pages.settings.value} element={<Settings />} />
-                </Routes>
-            </PublicLayout>
+            <RecoilRoot>
+                <PublicLayout>
+                    <Routes>
+                        <Route path={pages.home.value} element={<Home />} />
+                        <Route
+                            path={pages.statistics.value}
+                            element={<Statistics />}
+                        />
+                        <Route
+                            path={pages.currency.value}
+                            element={<Currency />}
+                        />
+                        <Route
+                            path={pages.settings.value}
+                            element={<Settings />}
+                        />
+                    </Routes>
+                </PublicLayout>
+            </RecoilRoot>
         </div>
     );
 }
