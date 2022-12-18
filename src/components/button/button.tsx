908cda79fa68ff1cc7ddcb6285fdc475cee8ba9e
Aleksandr_Ruzaev <aleksandr.ruzaev@rokolabs.com>
2022-12-18 18:20:51 +0400

    Add initial button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,29 @@
+type ButtonType = 'button' | 'submit';
+type ButtonProps = {
+    onClick?: () => void;
+    label?: string;
+    buttonType?: ButtonType;
+    icon: unknown;
+};
+
+const Button: React.FC<ButtonProps> = ({
+    onClick,
+    label,
+    buttonType = 'button',
+    icon,
+}): JSX.Element => {
+    const handleClick = (): void => {
+        onClick && onClick();
+    };
+
+    return (
+        <button onClick={handleClick} type={buttonType}>
+            <>
+                {label && <label>{label}</label>}
+                {icon && <div>{icon as any}</div>}
+            </>
+        </button>
+    );
+};
+
+export { Button };
