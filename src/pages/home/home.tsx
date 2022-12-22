c0efdedf8aab1b96d8ab5bfb932b247d7da62ec6
Aleksandr_Ruzaev <aleksandr.ruzaev@rokolabs.com>
2022-12-22 13:35:56 +0400

    set initial home layout

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -8,6 +8,7 @@ import {
 import { rates } from './home.store';
 import { SourceList } from './source-list/source-list';
 import { SpendingList } from './spending-list/spending-list';
+import './home.scss';
 
 const Home: React.FC = (): JSX.Element => {
     const compareCurrencies = useRecoilValue(currunciesToCompare);
@@ -15,19 +16,25 @@ const Home: React.FC = (): JSX.Element => {
     const currenciesRates = useRecoilValue(rates);
 
     return (
-        <>
-            <h3>{homeStrings.currency}</h3>
-            <p>{currency.displayName}</p>
-            <h3>{homeStrings.currenciesToCompare}</h3>
-            {compareCurrencies.map((currency) => (
-                <div key={currency.value}>
-                    <p>{currency.displayName}</p>
-                    <p>{currenciesRates[currency.value]}</p>
-                </div>
-            ))}
-            <SourceList />
-            <SpendingList />
-        </>
+        <div className="home">
+            <div className='home__info'>
+                <h3>{homeStrings.currency}</h3>
+                <p>{currency.displayName}</p>
+                <h3>{homeStrings.currenciesToCompare}</h3>
+                {compareCurrencies.map((currency) => (
+                    <div key={currency.value}>
+                        <p>{currency.displayName}</p>
+                        <p>{currenciesRates[currency.value]}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="home__source">
+                <SourceList />
+            </div>
+            <div className="home__spending">
+                <SpendingList />
+            </div>
+        </div>
     );
 };
 
